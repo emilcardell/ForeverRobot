eb9acb1e469665d396d814ac68b01ff907394b96
Niklas Jalmerud <roadkill03@gmail.com>
2012-06-11 18:35:56 +0200

    Comman

---
+++ ForeverRobot.RobotCommands/Source/ForeverRobot.RobotCommands/DropRobot/RobotDroppedEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Raven.Client;
+using TinyHandler;
+
+
+namespace ForeverRobot.RobotCommands.DropRobot
+{
+    public class RobotDroppedEventHandler: HandlerModule<RobotDroppedEvent>
+    {
+
+        public RobotDroppedEventHandler()
+        {
+            
+        }
+
+        public RobotDroppedEventHandler(IDocumentStore documentStore)
+        {
+            Process = robotDroppedEvent =>
+            {
+                using (var documentSession = documentStore.OpenSession())
+                {
+                    documentSession.Store(robotDroppedEvent);
+                    documentSession.SaveChanges();
+                }
+            };
+        }
+    }
+}
---
+++ ForeverRobot.RobotCommands/Source/ForeverRobot.RobotCommands/Infrastructure/ApplicationBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using ForeverRobot.Robots.CreateRobot;
+using ForeverRobot.Robots.Infrastructure;
+using Nancy.Bootstrappers.StructureMap;
+using TinyHandler;
+
+namespace ForeverRobot.Robots.Infrastructure
+{
+    public class ApplicationBootstrapper : StructureMapNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(StructureMap.IContainer existingContainer)
+        {
+            base.ConfigureApplicationContainer(existingContainer);
+            existingContainer.Configure(x => x.AddRegistry<RavenDbBootstrapper>());
+            existingContainer.Configure(x => x.For<HandlerModule<RobotCreatedEvent>>().Use<RobotCreatedEventHandler>());
+            HandlerCentral.Container = existingContainer;
+        }
+    }
+}
